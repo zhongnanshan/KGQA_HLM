69c8cd94abed595b01dde0a120c6ba3080639af2
zhongnanshan <beowulf.zhang@gmail.com>
2020-02-04 13:25:19 +0800

    实现直接存数据库导出紧凑格式的json数据文件，该json文件索引数据库中的所有节点，并导出所有节点对应的所有关系。

--- neo_db/post_proc_graph_db.py
+++ neo_db/post_proc_graph_db.py
@@ -1,7 +1,7 @@
 from config import graph
 import json
 
-def get_nodes():
+def test_get_nodes():
     ret = graph.run("MATCH (n:Person)-[r]->(m:Person) RETURN n,r,m")
     for record in ret:
         print("{0}-{1}->{2}".format(record['n']['Name'], record['r']['relation'], record['m']['Name']))
@@ -15,6 +15,7 @@ def test_graph_json():
             p2 = item_dict['p2']
             print("{0}-{1}->{2}".format(p1['properties']['Name'], r['type'], p2['properties']['Name']))
 
+# 从relation1.json生成relation2.json
 def post_proc_graph_json():
     new_json_data = []
     with open('./raw_data/relation1.json', encoding='utf-8-sig') as fd:
@@ -32,8 +33,42 @@ def post_proc_graph_json():
     with open('./raw_data/relation2.json', mode='w', encoding='utf-8') as fd:
         json.dump(new_json_data, fd, indent=True, ensure_ascii=False)
 
+def get_all_nodes():
+    nodes = graph.run("MATCH (n) RETURN n.Name")
+    return nodes
+
+def get_node_relation(node_name):
+    relations = graph.run("""MATCH (n)-[r]->(m)
+                            WHERE n.Name='{0}'
+                            RETURN r,m.Name""".format(node_name))
+    rel = []
+    for relation in relations:
+        rel_data = {'relation': relation['r'], 'node': relation['m.Name']}
+        rel.append(rel_data)
+    return rel
+
+def get_node(node_name):
+    node = graph.run("""MATCH (n) 
+                        WHERE n.Name='{0}' 
+                        RETURN labels(n) AS labels, n AS properties""".format(node_name))
+    data = node.next()
+    return {'labels': data['labels'], 'properties': data['properties']}
+
+# 从Neo4j数据库生成relation3.json
+def post_proc_graph_db():
+    nodes = get_all_nodes()
+    json_data = []
+    for record in nodes:
+        node = get_node(record['n.Name'])
+        relations = get_node_relation(record['n.Name'])
+        data = {'node': node, 'relations': relations}
+        json_data.append(data)
+    
+    with open('./raw_data/relation3.json', mode='w', encoding='utf-8') as fd:
+        json.dump(json_data, fd, ensure_ascii=False, indent=1)
 
 if __name__ == '__main__':
     # get_nodes()
     # test_graph_json()
-    post_proc_graph_json()
+    # post_proc_graph_json()
+    post_proc_graph_db()
