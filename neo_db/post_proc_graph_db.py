108c62a7f67fa6e37414450719ec5adf44b6cc4c
zhongnanshan <beowulf.zhang@gmail.com>
2020-02-04 20:56:58 +0800

    在post_proc_graph_db.py中添加了两种使用模式： 1.初始自动生成ID的功能； 2.使用Neo4j中已有数据库的功能；

--- neo_db/post_proc_graph_db.py
+++ neo_db/post_proc_graph_db.py
@@ -38,7 +38,7 @@ def get_all_nodes():
     nodes = graph.run("MATCH (n) RETURN n.Name")
     return nodes
 
-def get_node_relation(node_name):
+def get_node_relation_no_id(node_name):
     relations = graph.run("""MATCH (n)-[r]->(m)
                             WHERE n.Name='{0}'
                             RETURN r,m.Name""".format(node_name))
@@ -48,7 +48,17 @@ def get_node_relation(node_name):
         rel.append(rel_data)
     return rel
 
-def get_node(node_name):
+def get_node_relation_with_id(node_name):
+    relations = graph.run("""MATCH (n)-[r]->(m)
+                            WHERE n.Name='{0}'
+                            RETURN r,m.Name,m.ID""".format(node_name))
+    rel = []
+    for relation in relations:
+        rel_data = {'relation': relation['r'], 'node': {'Name': relation['m.Name'], 'ID': relation['m.ID']}}
+        rel.append(rel_data)
+    return rel
+
+def get_node_cal_id(node_name):
     node = graph.run("""MATCH (n) 
                         WHERE n.Name='{0}' 
                         RETURN labels(n) AS labels, n AS properties""".format(node_name))
@@ -56,6 +66,17 @@ def get_node(node_name):
     dt = {'labels': data['labels'], 'properties': data['properties']}
     dt_hash = my_hash(dt)
     dt['properties']['ID'] = dt_hash
+    return dt
+
+def get_node_no_cal_id(node_name):
+    node = graph.run("""MATCH (n) 
+                        WHERE n.Name='{0}' 
+                        RETURN labels(n) AS labels, n AS properties""".format(node_name))
+    data = node.next()
+    dt = {'labels': data['labels'], 'properties': data['properties']}
+    # 不需要再重新计算hash值
+    # dt_hash = my_hash(dt)
+    # dt['properties']['ID'] = dt_hash
     return dt
 
 # 同步关系目标节点的ID号(如果有则不修改。尽在初次生成Neo4j导入数据时使用)
@@ -77,17 +98,21 @@ def update_id(json_data):
 
     return new_json_data
 
+# 为了保持接口一致创建的简单函数
+def no_update_id(json_data):
+    return json_data
+
 # 从Neo4j数据库生成relation3.json
-def post_proc_graph_db():
+def post_proc_graph_db(get_node_fcn, get_node_relation_fcn, update_id_fcn):
     nodes = get_all_nodes()
     json_data = []
     for record in nodes:
-        node = get_node(record['n.Name'])
-        relations = get_node_relation(record['n.Name'])
+        node = get_node_fcn(record['n.Name'])
+        relations = get_node_relation_fcn(record['n.Name'])
         data = {'node': node, 'relations': relations}
         json_data.append(data)
-    
-    new_json_data = update_id(json_data)
+
+    new_json_data = update_id_fcn(json_data)
 
     with open('./raw_data/relation3.json', mode='w', encoding='utf-8') as fd:
         json.dump(new_json_data, fd, ensure_ascii=False, indent=1)
@@ -96,4 +121,7 @@ if __name__ == '__main__':
     # get_nodes()
     # test_graph_json()
     # post_proc_graph_json()
-    post_proc_graph_db()
+    # 用于初始创建Neo4j数据库时使用(自动更具节点属性计算节点ID)
+    # post_proc_graph_db(get_node_cal_id, get_node_relation_no_id, update_id)
+    # 用于已经存在节点ID的Neo4j数据库数据导出使用
+    post_proc_graph_db(get_node_no_cal_id, get_node_relation_with_id, no_update_id)
